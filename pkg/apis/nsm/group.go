dfd052184759caf9e2479b27440f5110cb6ceadc
alexandre <adcmenezes@gmail.com>
2020-08-28 09:18:39 -0400

    old layout cleanup

--- pkg/apis/nsm/group.go
+++
@@ -1,6 +0,0 @@
-// Package nsm contains nsm API versions.
-//
-// This file ensures Go source parsers acknowledge the nsm package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package nsm
